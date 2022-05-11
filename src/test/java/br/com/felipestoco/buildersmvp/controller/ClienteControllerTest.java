ae963e9cbd459646ae31bd7ffa5b4099ae57b161
bystoco <bystoco@gmail.com>
2022-05-10 21:21:51 -0300

    Ajuste testes controller

--- src/test/java/br/com/felipestoco/buildersmvp/controller/ClienteControllerTest.java
+++ src/test/java/br/com/felipestoco/buildersmvp/controller/ClienteControllerTest.java
@@ -2,6 +2,7 @@ package br.com.felipestoco.buildersmvp.controller;
 
 import br.com.felipestoco.buildersmvp.controller.form.ClienteForm;
 import br.com.felipestoco.buildersmvp.domain.Cliente;
+import br.com.felipestoco.buildersmvp.domain.dto.ClienteDTO;
 import br.com.felipestoco.buildersmvp.repository.ClienteRepository;
 import br.com.felipestoco.buildersmvp.service.ClienteService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,9 +19,11 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
@@ -28,10 +32,12 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -42,6 +48,7 @@ class ClienteControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    private AutoCloseable autoCloseable;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -56,6 +63,7 @@ class ClienteControllerTest {
 
     @BeforeEach
     void setUp() throws Exception {
+        autoCloseable = MockitoAnnotations.openMocks(this);
 
         final Cliente cli1 = new Cliente("Cliente01", "Sobrenome", LocalDate.of(1981, 11, 18), "cliente01@email.com", "999999");
         cli1.setId(1L);
@@ -76,7 +84,6 @@ class ClienteControllerTest {
                 .andDo(print())
                 .andExpect(jsonPath("$.content", hasSize(3)))
                 .andReturn();
-
     }
 
     @Test
@@ -85,16 +92,20 @@ class ClienteControllerTest {
         final Cliente novoCliente = new Cliente("Novo", "Sobrenome", LocalDate.of(2002, 7, 17), "cliente03@email.com", "999999");
         novoCliente.setId(4L);
         final ClienteForm clienteForm = new ClienteForm("Novo", "Sobrenome", LocalDate.of(2002, 7, 17), "cliente03@email.com", "999999");
-        when(clienteService.salvar(clienteForm)).thenReturn(novoCliente);
+        when(clienteService.salvar(Mockito.any(ClienteForm.class))).thenReturn(novoCliente);
 
-        this.mockMvc.perform(post(url)
+        final MvcResult mvcResult = this.mockMvc.perform(post(url)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(clienteForm))
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string("Felipe"))
+                .andExpect(status().isCreated())
                 .andReturn();
+
+        final MockHttpServletResponse response = mvcResult.getResponse();
+        final ClienteDTO clienteSalvo = objectMapper.readValue(response.getContentAsString(), ClienteDTO.class);
+        assertEquals(novoCliente.getNome(), clienteSalvo.getNome());
+        assertEquals(response.getRedirectedUrl(), "http://localhost/clientes/4");
     }
 
     @Test
